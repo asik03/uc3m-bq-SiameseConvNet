1aa33dbca6ca161039b0cab74bc426fc30866960
Asier <asier.alcaide@yahoo.es>
2018-06-20 21:47:51 +0200

    Cleaning code

--- create_diff_dataset.py
+++ create_diff_dataset.py
@@ -1,14 +1,41 @@
+'''Create a '''
+
+# Copyright (c) 2018 by BQ. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import random
 
-data_file = "./data/all_img_paths.txt"
-out_file = "./data/diff_dataset.txt"
+imgs_paths_txt_path = "./data/all_img_paths.txt"
+diff_dataset_txt_path = "./data/diff_dataset.txt"
 bottlenecks_dir = "./data/bottlenecks/"
 num_tuples_per_class = 30
 
 
 def main():
-    with open(out_file, 'w') as out:
+    with open(diff_dataset_txt_path, 'w') as out:
         for class_name in os.listdir(bottlenecks_dir):
             class_path = os.path.join(bottlenecks_dir, class_name)
             print(os.listdir(class_path))
--- pre_input.py
+++ pre_input.py
@@ -1,11 +1,40 @@
+# Copyright (c) 2018 by BQ. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
+import random
 import tensorflow as tf
 import numpy as np
 
+filtered_data_dir = './data/datasets/filtered_dataset'
+imgs_paths_txt_path = "./data/all_img_paths.txt"
+diff_dataset_txt_path = "./data/diff_dataset.txt"
+bottlenecks_dir = "./data/bottlenecks/"
 
-data_dir = '/home/uc3m1/PycharmProjects/siameseFaceNet/data/datasets/filtered_dataset'
-diff_dataset = "./data/diff_dataset.txt"
-img_paths = "./data/all_img_paths.txt"
+# It will generate n tuples with label "0" and other n tuples with label "1" for each class or person.
+num_tuples_per_class = 30
 
 
 def generate_txt_with_all_images(data_dir, path):
@@ -83,11 +112,57 @@ def _get_image_and_label_from_entry(entry):
     return file_path_1, file_path_2, int(label)
 
 
-def main():
-    #generate_txt_with_all_images(data_dir, img_paths)
+def create_diff_dataset_txt(diff_dataset_txt_path, bottlenecks_dir, num_tuples_per_class):
+    with open(diff_dataset_txt_path, 'w') as out:
+        for class_name in os.listdir(bottlenecks_dir):
+            class_path = os.path.join(bottlenecks_dir, class_name)
+            print(os.listdir(class_path))
 
-    generate_tfrecord_files(diff_dataset, "./data/tfrecord_train_file")
-    generate_tfrecord_files(diff_dataset, "./data/tfrecord_eval_file")
+            for i in range(num_tuples_per_class):
+                npy_path_1 = get_random_item_from_class(class_path)
+                npy_path_2 = get_random_item_from_class(class_path)
+
+                out.write(npy_path_1 + ' ' + npy_path_2 + ' 1' + '\n')
+                print(npy_path_1, npy_path_2, '1')
+
+            for i in range(num_tuples_per_class):
+                npy_path_1 = get_random_item_from_class(class_path)
+
+                comparing_class = get_random_class(bottlenecks_dir, exclude=os.listdir(bottlenecks_dir).index(class_name))
+                npy_path_2 = get_random_item_from_class(os.path.join(bottlenecks_dir, comparing_class))
+
+                out.write(npy_path_1 + ' ' + npy_path_2 + ' 0' + '\n')
+                print(npy_path_1, npy_path_2, '0')
+
+
+def get_random_item_from_class(class_path):
+    rand = int(random.random() * len(os.listdir(class_path)))
+    npy_path = os.listdir(class_path)[rand]
+
+    return os.path.join(class_path, npy_path)
+
+
+def get_random_class(basename_dir, exclude=None):
+    rand = int(random.random() * len(os.listdir(basename_dir)))
+
+    while rand == exclude:
+        rand = int(random.random() * len(os.listdir(basename_dir)))
+
+    class_path = os.listdir(basename_dir)[rand]
+
+    return class_path
+
+
+def main():
+    generate_txt_with_all_images(filtered_data_dir, imgs_paths_txt_path)
+
+    '''Create bottlenecks with "inferece_bottlecks.py first"'''
+
+    create_diff_dataset_txt(diff_dataset_txt_path, bottlenecks_dir, num_tuples_per_class)
+
+    generate_tfrecord_files(diff_dataset_txt_path, "./data/tfrecord_train_file")
+
+    generate_tfrecord_files(diff_dataset_txt_path, "./data/tfrecord_eval_file")
 
 
 if __name__ == '__main__':
